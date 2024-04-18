f5456c48111596e5e5595dd957d1b7f97a9a13e8
devmusiczz <onlyfordance0@gmail.com>
2024-04-18 11:58:52 +0530

    created redux folder, custom hooks file, redux logic slice feature and store.ts file for configure redux in the application

--- Question 2 React App/tailwind.config.js
+++ Question 2 React App/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#0989FF",
+        topHeadingPrimary: "#010f1c",
+        topHeadingSecondary: "#021d35",
+        pink: "#fd4b6b",
+      },
+    },
+    container: {
+      center: true,
+      padding: "15px"
+    },
   },
   plugins: [],
-}
+};
